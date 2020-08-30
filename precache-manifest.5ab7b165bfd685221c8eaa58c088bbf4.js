668eba6f7f889aabdf4a7d64ae171ecf118385af
loic-e <loic.ernult@outlook.fr>
2020-08-30 20:08:49 +0200

    Updates

---
+++ precache-manifest.5ab7b165bfd685221c8eaa58c088bbf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbdbdb372f27f137b497309be3e5b1b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ac00fdced93fdb9e6c1b.bundle.js"
+  },
+  {
+    "url": "runtime~main.ac00fdced93fdb9e6c1b.bundle.js"
+  },
+  {
+    "revision": "7b93943264bec820a350",
+    "url": "static/css/main.e2900a46.chunk.css"
+  },
+  {
+    "url": "vendors~main.ac00fdced93fdb9e6c1b.bundle.js"
+  },
+  {
+    "url": "vendors~main.ac00fdced93fdb9e6c1b.bundle.js.LICENSE.txt"
+  }
+]);
